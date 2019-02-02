282d2883466012654176a7935302bae7f01430c6
Alekssenko <sasha18081960@gmail.com>
2019-02-02 10:58:26 +0200

    02.02.19 start

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-// $(document).ready(function () {
+
 $('.player').slick({
-    // slidesToShow: 1,
-    // slidesToScroll: 1,
+
     arrows:true,
     appendArrows: $('.player'),
     prevArrow: '<div class="prev"><i class="fa fa-chevron-left"></i></div>',
@@ -10,19 +9,17 @@ $('.player').slick({
 });
 var button = document.getElementById('video_button');
 var video = document.getElementById('video-player');
-// var videoPlayer = document.getElementById('video');
 
 function playerGo() {
+
     if(video.hasAttribute('controls')){
         video.removeAttribute('controls');
         button.style.display = 'block';
-    }else {
+    }
+    else {
         video.setAttribute('controls', '');
         button.style.display ='none';
     }
 }
 button.onclick = playerGo;
-// video.onclick = function(){
-//     video.removeAttribute('controls');
-//     button.style.display ='block';
-// };
+
